c63dcb4ab3e499d791374389e25bcae0342337a0
douglasnobree <douglasclaudino543@gmail.com>
2024-11-19 13:58:03 -0300

    aceita ai

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './service/tickets.service'; 
 import { TicketsController } from './controller/tickets.controller'; 
+import { PrismaService } from 'src/utils/prisma.service';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService],
+  providers: [TicketsService, PrismaService],
 })
 export class TicketsModule {}
