32fed9b8edab63031f987b18606d1d96fc3b30a0
douglasnobree <douglasclaudino543@gmail.com>
2024-12-17 12:49:29 -0300

    adiciona envio de email ao fechar ticket e cria template HTML

--- src/tickets/service/tickets.service.ts
+++ src/tickets/service/tickets.service.ts
@@ -5,12 +5,16 @@ import { CreateTicketDto } from '../dto/create-ticket.dto';
 import { UpdateTicketDto } from '../dto/update-ticket.dto';
 import { UserDAO } from 'src/user/dao/user-dao';
 import { Tickets } from '@prisma/client';
+import { EmailService } from 'src/utils/mail.service';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class TicketsService {
   constructor(
     private readonly ticketDAO: TicketDAO,
-    private readonly userDAO: UserDAO
+    private readonly userDAO: UserDAO,
+    private readonly emailService: EmailService,
   ) {}
 
   async getAllTickets(): Promise<Tickets[]> {
@@ -41,14 +45,28 @@ export class TicketsService {
   }
 
   async updateTicket(id: string, updateTicketDto: UpdateTicketDto): Promise<Tickets> | undefined {
-    const ticket = await this.ticketDAO.findById(id);
-    if (!ticket) {
-      throw new BadRequestException('Ticket não encontrado.');
+    try{
+      const readHTML = () => {
+        const filePath = path.join(__dirname, 'emails', '../../../../src/utils/ticket-fechado.html'); 
+        return fs.readFileSync(filePath, 'utf-8'); // Lê o arquivo HTML e retorna como string
+      };
+      const ticket = await this.ticketDAO.findById(id);
+      if (!ticket) {
+        throw new BadRequestException('Ticket não encontrado.');
+      }
+      if (ticket.status === 'closed' && updateTicketDto.status !== 'in_progress') {
+        throw new BadRequestException('Tickets fechados não podem ser editados.');
+      }
+      if (ticket.status === 'in_progress' && updateTicketDto.status === 'closed') {
+        const htmlContent = readHTML();
+        const userEmail = await this.userDAO.findById(ticket.user_id);
+        await this.emailService.sendEmail(userEmail.email, 'Ticket fechado', 'Seu ticket foi fechado.', htmlContent);
+      }
+      return this.ticketDAO.update(id, updateTicketDto);
+
+    }catch(e){
+      throw new BadRequestException(e.message);
     }
-    if (ticket.status === 'closed' && updateTicketDto.status !== 'in_progress') {
-      throw new BadRequestException('Tickets fechados não podem ser editados.');
-    }
-    return this.ticketDAO.update(id, updateTicketDto);
   }
 
   async deleteTicket(id: string): Promise<boolean> {
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -4,9 +4,10 @@ import { TicketsController } from './controller/tickets.controller';
 import { PrismaService } from 'src/utils/prisma.service';
 import { TicketDAO } from './dao/ticket-dao';
 import { UserDAO } from 'src/user/dao/user-dao';
+import { EmailService } from 'src/utils/mail.service';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService, PrismaService, TicketDAO, UserDAO],
+  providers: [TicketsService, PrismaService, TicketDAO, UserDAO, EmailService],
 })
 export class TicketsModule {}
